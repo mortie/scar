476415c36a84190d2353b892d7317735bc01057d
Martin Dørum <martid0311@gmail.com>
2023-04-30 19:32:23 +0200

    fuzzing

---
+++ scar-rs/scar/fuzz/fuzz_targets/stat_files.rs
@@ -0,0 +1,35 @@
+#![no_main]
+
+use scar::read::ScarReader;
+use std::io::Cursor;
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    let cursor = Cursor::new(data.to_vec());
+    let mut r = match ScarReader::new(cursor) {
+        Ok(r) => r,
+        Err(_) => return,
+    };
+
+    let index = match r.index() {
+        Ok(index) => index,
+        Err(_) => return,
+    };
+
+    for entry in index {
+        let entry = match entry {
+            Ok(entry) => entry,
+            Err(_) => return,
+        };
+
+        let mut pr = match r.read_item(&entry) {
+            Ok(pr) => pr,
+            Err(_) => return,
+        };
+
+        match pr.next_header() {
+            Ok(_) => (),
+            Err(_) => return,
+        };
+    }
+});
